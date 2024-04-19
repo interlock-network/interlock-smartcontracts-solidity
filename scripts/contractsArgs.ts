031e23fde0cec3271ef60bd7126d3af0ead685dc
harzo <harzojunior@gmail.com>
2024-04-19 22:33:37 +0200

    build: save sepolia deploymen data

--- scripts/contractsArgs.ts
+++ scripts/contractsArgs.ts
@@ -3,6 +3,7 @@ type InterlockNetworkArgs = {
     initialOwner: `0x${string}`
   }
   proxyAdminOwner?: `0x${string}`
+  salt?: string
 }
 
 const contractsArgs: Record<
@@ -16,15 +17,17 @@ const contractsArgs: Record<
       params: {
         initialOwner: '0x4599Bb9B14e1bea536C175206cf878fe07dE390F'
       },
-      proxyAdminOwner: '0x4599Bb9B14e1bea536C175206cf878fe07dE390F'
+      proxyAdminOwner: '0x4599Bb9B14e1bea536C175206cf878fe07dE390F',
+      salt: 'InterlockNetworkOwnsYou'
     }
   },
-  arbitrumSepolia: {
+  baseSepolia: {
     InterlockNetwork: {
       params: {
-        initialOwner: '0xf0Da5D820b6d0B9383Bb8e3ED7D3144b32B1349F'
+        initialOwner: '0x4599Bb9B14e1bea536C175206cf878fe07dE390F'
       },
-      proxyAdminOwner: '0xf0Da5D820b6d0B9383Bb8e3ED7D3144b32B1349F'
+      proxyAdminOwner: '0x4599Bb9B14e1bea536C175206cf878fe07dE390F',
+      salt: 'InterlockNetworkOwnsYou'
     }
   }
 }
